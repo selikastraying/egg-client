78860b7e3b3c785302d52102ddab5e3bf4db157e
selikastraying <selikastraying@gmail.com>
2021-06-23 14:57:18 +0800

    fix little problem

--- src/services/NetServices.js
+++ src/services/NetServices.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  // baseURL: 'http://localhost:7001/api/posts',
-  baseURL: 'http://web:7001/api/posts',
+  baseURL: 'http://localhost:7001/api/posts',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
